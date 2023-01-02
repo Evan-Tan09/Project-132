2ae1040a618a9e26e73e4378a0bea97395404d23
Evan-Tan09 <84255090+Evan-Tan09@users.noreply.github.com>
2023-01-02 09:35:19 +0800

    Add files via upload

---
+++ page3.js
@@ -0,0 +1,47 @@
+img = "";
+status = "";
+detectedAreaWidth = "";
+detectedAreaHeight = "";
+detectedAreaX = "";
+detectedAreaY = "";
+detectedAreaLabel = "";
+function setup() {
+    canvas = createCanvas(800, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+function modelLoaded() {
+    console.log("Model loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        detectedAreaWidth = results[0].width;
+        detectedAreaHeight = results[0].height;
+        detectedAreaX = results[0].x;
+        detectedAreaY = results[0].y;
+        detectedAreaLabel = results[0].label;
+        console.log("X: " + detectedAreaX);
+        console.log("Y: " + detectedAreaY);
+        console.log("Width: " + detectedAreaWidth);
+        console.log("Height: " + detectedAreaHeight);
+        console.log("Label: " + detectedAreaLabel);
+        rect(detectedAreaX, detectedAreaY, detectedAreaWidth, detectedAreaHeight);
+        text(detectedAreaLabel, detectedAreaX + 10, detectedAreaY + 10);
+    }
+}
+function preload() {
+    img = loadImage("pen.JPG");
+}
+function draw() {
+    image(img, 0, 0, 800, 400);
+    noFill();
+    stroke("#ff00ff");
+    rect(60, 100, 490, 290);
+    text("knife", 70, 110);
+}
